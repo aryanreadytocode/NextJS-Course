d24d5042a06fa55434a22bd242b242ab36515026
Raj Aryan <arpittaryan2k12@gmail.com>
2025-02-20 22:39:31 +0530

    Revert "dynamic layout"
    This reverts commit e5889474746977f4d512916a82f748b5f3d2c4dd.

--- blog/src/app/studentlist/[chhatra]/page.js
+++
@@ -1,9 +0,0 @@
-'use client';
-export default function StudentDetails({params}) {
-    console.log(params); 
-  return (
-    <div style={{ margin: "20px" }}>
-      <h1>Student Detail</h1>
-    </div>
-  );
-}
