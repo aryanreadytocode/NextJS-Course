935397f9f048880e6617d791d748855bfb143b90
Raj Aryan <arpittaryan2k12@gmail.com>
2025-02-26 22:48:56 +0530

    next image

--- blog/src/app/not-found.js
+++
@@ -1,7 +0,0 @@
-export default function NotFound(){
-    return (
-        <div>
-            <h1>This is not found page</h1>
-        </div>
-    );
-}
--- blog/src/app/page.js
+++ blog/src/app/page.js
@@ -1,12 +1,20 @@
-import custom from './custom.module.css'
-
+import Image from "next/image"
+import Profile from '../../public/next.svg'
 
 export default function Home() {
+  console.log(Profile)
     return(
       <main>
-        <h1 className={custom.main}>CSS module with Next Js</h1>
-        <h2 className="main">module 2</h2>
-        <p className="main">This is paragraph</p>
+        <h1>Image optimization in Next</h1>
+        {/* <Image src={Profile} alt="Image"/> */}
+        {/* <img src={Profile.src}/> */}
+
+        <Image 
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/220px-Image_created_with_a_mobile_phone.png"
+        alt="Image"
+        width={1800}
+        height={1800}/>
+
       </main>
     )
 }
