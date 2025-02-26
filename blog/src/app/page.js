dd338f17c3994d227112bc942520ba230f8dba51
Raj Aryan <arpittaryan2k12@gmail.com>
2025-02-26 22:09:45 +0530

    module with next

--- blog/src/app/page.js
+++ blog/src/app/page.js
@@ -1,9 +1,12 @@
-import Link from "next/link";
+import custom from './custom.module.css'
+
 
 export default function Home() {
     return(
       <main>
-        <h1>Style and CSS with Next Js</h1>
+        <h1 className={custom.main}>CSS module with Next Js</h1>
+        <h2 className="main">module 2</h2>
+        <p className="main">This is paragraph</p>
       </main>
     )
 }
