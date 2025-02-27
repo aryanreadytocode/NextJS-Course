4a49fc7d59b2f418a254bfde363b6a52e3515382
Raj Aryan <arpittaryan2k12@gmail.com>
2025-02-27 12:05:22 +0530

    comment

--- blog/src/app/page.js
+++ blog/src/app/page.js
@@ -1,16 +1,9 @@
-import { Roboto } from "next/font/google"
 
-const roboto = Roboto({
-  weight:'100',
-  subsets:['latin'],
-  display:'swap'
-})
 export default function Home() {
     return(
       <main>
-        <h1>Font optimization in Next</h1>
-        {/* <h1 style={{fontFamily:'Roboto', fontWeight:100}}>Font with link Tag in Next</h1> */}
-      <h1 className={roboto.className}>Font with next js font feature</h1>
+        <h1>Static Assets in Next</h1>
+        <img src="/kf.jpg"/>
       </main>
     )
 }
