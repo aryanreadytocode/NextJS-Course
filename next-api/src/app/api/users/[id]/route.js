4af7a6c70ddbb93436df6727c2d78842bf85ab3b
Raj Aryan <arpittaryan2k12@gmail.com>
2025-02-28 09:23:32 +0530

    custom message when data not found

--- next-api/src/app/api/users/[id]/route.js
+++ next-api/src/app/api/users/[id]/route.js
@@ -3,9 +3,10 @@ import { NextResponse } from "next/server";
 
 export async function GET(req, content) {
     const param = await (content.params)
-    const data = user.filter((item) => 
+    var data = user.filter((item) => 
         item.id == param.id
     )
+    data = data.length>0 ? data : {"result": "No Data", "success": false} 
     console.log(data)
     return NextResponse.json(data, {status:200})
 }
