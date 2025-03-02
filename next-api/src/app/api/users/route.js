d56b58a110c0bbbd24420c65c020dd48f64f2733
Raj Aryan <arpittaryan2k12@gmail.com>
2025-03-02 16:53:10 +0530

    post api

--- next-api/src/app/api/users/route.js
+++ next-api/src/app/api/users/route.js
@@ -5,3 +5,12 @@ export function GET() {
     const data = user;
     return NextResponse.json(data, {status:200})
 }
+
+export async function POST(request) {
+    let payload = await request.json();
+    if(!payload.name || !payload.email || !payload.age) {
+        return NextResponse.json({result: "Data not found"}, {status:400})
+    }
+    return NextResponse.json({result: "Data saved success"}, {status:200})
+}
+
